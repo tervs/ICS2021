b316418dbbebc7ace2583de9800de9e8837ed08b
tracer-ics2021 <tracer@njuics.org>
2021-09-19 21:59:08 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 264d1be95d4f415f8ac6706f32afc717   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:59:08 up 32 min,  1 user,  load average: 3.40, 2.35, 1.84

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -65,7 +65,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  {"si N","single step",cmd_si},
+  {"si","single step",cmd_si},
   /* TODO: Add more commands */
 
 };
