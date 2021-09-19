f2b95d26421c53fa33a49e3bc7faf71fd6f5d109
tracer-ics2021 <tracer@njuics.org>
2021-09-19 22:51:37 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 264d1be95d4f415f8ac6706f32afc717   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:51:37 up  1:25,  1 user,  load average: 0.69, 0.35, 0.41

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -58,11 +58,15 @@ return 0;
 static int cmd_info(char *args) 
 {
      char *arg = strtok(NULL, " ");
-	if(strcmp(arg,"r")){
+     if(strcmp(arg,"r")==0)
+     printf("ok");
+     return 0;
+     
+	/*if(strcmp(arg,"r")){
 isa_reg_display();
 }
 return 0;
-
+*/
 }
 
 
