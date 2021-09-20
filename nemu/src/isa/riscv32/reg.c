e4bd1dfec7c29f6605f54a9aafd9dc1fffc8776c
tracer-ics2021 <tracer@njuics.org>
2021-09-20 09:43:04 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 0650eb9626534dff8f3b1551ed84b7bf   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:43:04 up 33 min,  1 user,  load average: 0.60, 0.55, 0.45

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -22,16 +22,33 @@ printf("%s  %d   |","t0",isa_reg_str2val("t0", success));
 printf("%s  %d   |","t1",isa_reg_str2val("t1", success));
 printf("%s  %d \n","t2",isa_reg_str2val("t2", success));
 printf("\n");
-printf("%s  %d | ","$0",isa_reg_str2val("$0", success));
-printf("%s  %d | ","ra",isa_reg_str2val("ra", success));
-printf("%s  %d | ","sp",isa_reg_str2val("sp", success));
-printf("%s  %d | ","gp",isa_reg_str2val("gp", success));
-printf("%s  %d | ","tp",isa_reg_str2val("tp", success));
-printf("%s  %d | ","t0",isa_reg_str2val("t0", success));
-printf("%s  %d | ","t1",isa_reg_str2val("t1", success));
-printf("%s  %d \n","t2",isa_reg_str2val("t2", success));
-
-
+printf("%s  %d   |","s0",isa_reg_str2val("s0", success));
+printf("%s  %d   |","s1",isa_reg_str2val("s1", success));
+printf("%s  %d   |","a0",isa_reg_str2val("a0", success));
+printf("%s  %d   |","a1",isa_reg_str2val("a1", success));
+printf("%s  %d   |","a2",isa_reg_str2val("a2", success));
+printf("%s  %d   |","a3",isa_reg_str2val("a3", success));
+printf("%s  %d   |","a4",isa_reg_str2val("a4", success));
+printf("%s  %d \n","a5",isa_reg_str2val("a5", success));
+printf("\n");
+printf("%s  %d   |","a6",isa_reg_str2val("a6", success));
+printf("%s  %d   |","a7",isa_reg_str2val("a7", success));
+printf("%s  %d   |","s2",isa_reg_str2val("s2", success));
+printf("%s  %d   |","s3",isa_reg_str2val("s3", success));
+printf("%s  %d   |","s4",isa_reg_str2val("s4", success));
+printf("%s  %d   |","s5",isa_reg_str2val("s5", success));
+printf("%s  %d   |","s6",isa_reg_str2val("s6", success));
+printf("%s  %d \n","s7",isa_reg_str2val("s7", success));
+printf("\n");
+printf("%s  %d   |","s8",isa_reg_str2val("s8", success));
+printf("%s  %d   |","s9",isa_reg_str2val("s9", success));
+printf("%s  %d   |","s10",isa_reg_str2val("s10", success));
+printf("%s  %d   |","s11",isa_reg_str2val("s11", success));
+printf("%s  %d   |","t3",isa_reg_str2val("t3", success));
+printf("%s  %d   |","t4",isa_reg_str2val("t4", success));
+printf("%s  %d   |","t5",isa_reg_str2val("t5", success));
+printf("%s  %d \n","t6",isa_reg_str2val("t6", success));
+printf("\n");
 }
 
 
