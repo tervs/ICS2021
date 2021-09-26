b12276ed0144067d6283df33e6975032f1564078
tracer-ics2021 <tracer@njuics.org>
2021-09-26 15:29:10 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 1092f167c16942ed8168cda8fde65805   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:29:10 up 51 min,  1 user,  load average: 0.65, 0.83, 0.87

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -67,6 +67,11 @@ return 0;
 static int cmd_x(char *args) 
 {
   char *N = strtok(NULL, " ");
+  if(strcmp(N," ")==0)
+  {
+    printf("wrong");
+    exit(0);
+  }
 uint32_t n=atoi(N);
      char *arg = strtok(NULL, " ");
      uint32_t addr=strtol(arg,NULL,16);
