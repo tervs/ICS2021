61a1301f18a089bc2a6a72fa6719ea88fc5da5ff
tracer-ics2021 <tracer@njuics.org>
2021-09-27 22:33:48 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: a6e6d25f04e94fcf9b48a13020b70b02   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:33:48 up  5:52,  1 user,  load average: 1.56, 1.84, 1.72

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -105,9 +105,10 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 
-        switch (rules[i].token_type) {
+        /*switch (rules[i].token_type) {
           default: TODO();
         }
+        */
 
         break;
       }
