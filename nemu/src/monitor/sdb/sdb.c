733e8784f8685d21da015901e8016f136037bab4
tracer-ics2021 <tracer@njuics.org>
2021-09-28 20:22:11 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 4ac6904baf664eb897f5ae78f641f336   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:22:11 up  1:52,  1 user,  load average: 0.39, 0.65, 0.72

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -44,7 +44,8 @@ static int cmd_p(char *args) {
   bool success;
   success=true;
   x=expr(EXPR, &success);
-  printf("%d\n",x);
+  if(success){printf("Bad Expression");}
+  else {printf("%d\n",x);}
   //printf("%d   %s",tokens[0].type,tokens[0].token_type);
   //printf("%s", EXPR);
   return 0;
