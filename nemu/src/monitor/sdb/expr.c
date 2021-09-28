4a0bd6e663000327762279a23a75bbf14f0c7226
tracer-ics2021 <tracer@njuics.org>
2021-09-28 09:15:05 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 618963b100014fa1af584445ee68db50   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:15:05 up 18 min,  1 user,  load average: 0.20, 0.42, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -180,11 +180,11 @@ int op_position(int p,int q)
     {
        if(tokens[i].type=='+'||tokens[i].type=='-')
       {
-          x=i;
+          x=i+99;
       }
       else  if((tokens[i].type=='*'||'/')&&(tokens[x].type!='+'||'-'))
       {
-        x=i;
+        x=i+10;
       }
       else break;
       Log("now x is %d while  i is %d and type is %d",x,i,tokens[i].type);
