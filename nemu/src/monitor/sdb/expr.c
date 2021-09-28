e8f8559cc3ab599df59ba1ff74ced2fd47b114db
tracer-ics2021 <tracer@njuics.org>
2021-09-28 11:43:48 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 618963b100014fa1af584445ee68db50   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:43:49 up  2:47,  1 user,  load average: 0.34, 0.63, 0.92

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -96,11 +96,11 @@ static bool make_token(char *e) {
         
         if(i!=TK_NOTYPE)
         {
-      tokens[position-1].type=i;
+        tokens[position-1].type=i;
         strcpy(tokens[position-1].str,rules[i].regex);
         }
 
-       Log("%d   %d  %s ",position,tokens[position-1].type,tokens[position-1].str);
+       Log("now position is %d  type here is %d   str is %s ",position-1,tokens[position-1].type,tokens[position-1].str);
           break;
       }
     }
