c3ac6d066596d1511e423793715a410d4b13639b
tracer-ics2021 <tracer@njuics.org>
2021-09-30 01:27:24 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: f56838d225d347318cea3d0cd01637cc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  01:27:24 up 12:58,  1 user,  load average: 1.05, 0.93, 0.84

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -167,6 +167,7 @@ static bool make_token(char *e)
       if (i == NR_REGEX) 
       {
         printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+        sign=3;
         return false;//sign
       }
      // Log("%d",j);
