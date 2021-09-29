642566e8a24db1058729497becb96095116d210f
tracer-ics2021 <tracer@njuics.org>
2021-09-29 10:37:42 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 63ae798d0b0c428196de130b1c3ccd73   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:37:42 up 48 min,  1 user,  load average: 0.35, 0.20, 0.24

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -108,8 +108,8 @@ int j=0;
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-          //  i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
         
@@ -121,7 +121,7 @@ int j=0;
         }
         else{Log("warning of space");break;}
 
-      // Log("now position is %d  type here is %d   str is %s ",j-1,tokens[j].type,tokens[j-1].str);
+       Log("now position is %d  type here is %d   str is %s ",j-1,tokens[j].type,tokens[j-1].str);
           break;
       }
 
