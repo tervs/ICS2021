962531f9db214e2c4ef7364f3ae1e8a5390a41f0
tracer-ics2021 <tracer@njuics.org>
2021-09-29 21:11:06 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: f56838d225d347318cea3d0cd01637cc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:11:06 up  8:41,  1 user,  load average: 0.86, 1.15, 1.66

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,9 +1,8 @@
 #include <isa.h>
 #include <regex.h>
 #include <stdbool.h>
-
+uint32_t *sign=0;
 int t;
-uint32_t sign;
 //bool p=true;
 //bool *success=&p;
 //expr总体用一个sucess返回秋值是否成功，所以各个部分自己的bool变量应该记录做别名
@@ -149,7 +148,7 @@ static bool make_token(char *e)
                   uint64_t temp=isa_reg_str2val(tokens[j-1].str,&is_reg_exist);
                   tokens[j-1].type=258;
                   if(!is_reg_exist){
-                    sign=1;
+                    *sign=1;
                     Log("%d\n",is_reg_exist);
                     return false;} //sign
                   //printf("%ld\n",temp);
@@ -180,7 +179,7 @@ static bool make_token(char *e)
 
 
 
-word_t expr(char *e, bool *success,uint32_t sign) 
+word_t expr(char *e, bool *success,uint32_t *sign) 
 
 {
   //bool *success;
@@ -190,7 +189,7 @@ word_t expr(char *e, bool *success,uint32_t sign)
   {
     *success = false;
      Log("%d",*success);
-      Log("%d",sign);
+      Log("%d",*sign);
     return 0;
   }
  
