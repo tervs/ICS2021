d86b87a854c89886544551acac65915bad3619ac
tracer-ics2021 <tracer@njuics.org>
2021-10-01 17:44:01 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 3329056212864549ac5b4205eddc4d14   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  17:44:01 up  2:11,  1 user,  load average: 0.51, 0.87, 0.83

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -260,6 +260,7 @@ word_t  eval(int p, int q,bool *success)
 
       else 
       {
+        Log("?");
       int op = op_position(p,q);
       if(op==-1){exit(0);}
       Log("op is %d  p is %d  q is  %d",op,p,q);
