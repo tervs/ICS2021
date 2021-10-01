8dec01872f5285269fa7d5b8820d77d78f695be8
tracer-ics2021 <tracer@njuics.org>
2021-10-02 00:36:46 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: eeaa1beb752441888fea0895431597d3   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  00:36:46 up  2:27,  1 user,  load average: 0.60, 0.57, 0.51

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -2,13 +2,21 @@
 
 #define NR_WP 32
 
+
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
+  char *EXPR;
+  word_t x;//初始值
+  int last;
 
   /* TODO: Add more members if necessary */
 
 } WP;
+
+WP* new_wp();
+void free_wp(WP *wp);
+
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
@@ -27,3 +35,29 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp()
+{
+  if(free_==NULL){printf("no free watchpoint");return 0;}
+  else
+  {
+
+    return free_;
+  }
+}
+void free_wp(WP *wp);
+
+void delete(WP *linklist,int n)
+{
+  bool empty=false;
+  for(int i=0;i<NR_WP;i++)
+  {
+    if(wp_pool[i].next==&wp_pool[n])
+    {
+      wp_pool[i].next=wp_pool[n].next;
+      empty=false;
+    }
+  }
+  if(empty){linklist=NULL;}
+
+  
+}
