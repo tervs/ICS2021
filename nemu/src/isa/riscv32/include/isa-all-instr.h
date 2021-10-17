e80c1eee63d0d2b26cba41595d5e80af6da63806
tracer-ics2021 <tracer@njuics.org>
2021-10-17 02:44:16 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 3e82dbae858a479eb560c5ce5e4654a0   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  02:44:16 up  3:09,  1 user,  load average: 1.29, 1.00, 0.84

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,6 +1,7 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap)
+#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(addi) 
+//f(slti) f(sltiu) f(xori)\f(ori) f(andi) 
 
 def_all_EXEC_ID();
