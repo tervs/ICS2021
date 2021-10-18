78d0b3305246d20fcfbcb57efe016e416d221c3b
tracer-ics2021 <tracer@njuics.org>
2021-10-18 00:34:48 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 691800fbb3874dd7881141f83df636a0   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  00:34:48 up  2:18,  1 user,  load average: 0.76, 0.70, 0.67

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -1,16 +1,19 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
-
+extern const char *regs[];
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
-printf("test\n"); 
 
-  bool s=difftest_check_reg("s0",pc,(*ref_r).gpr[0]._32,gpr(0));
+
+  for(int i=0;i<32;i++)
+  {
+     bool s=difftest_check_reg(regs[i],pc,(*ref_r).gpr[i]._32,gpr(i));
+     if(!s){return false;}
+  }
+ return true;
   
- printf("test\n"); 
-  return s;
-}
 
+}
 void isa_difftest_attach() {
 }
