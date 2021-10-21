c3794b30a12f0a741941dc4046f014b6f8a09047
tracer-ics2021 <tracer@njuics.org>
2021-10-21 06:56:57 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 22753a62701248fe81ea6016870c68f1   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  06:56:57 up  1:29,  1 user,  load average: 0.52, 0.64, 0.70

--- abstract-machine/am/src/platform/nemu/include/nemu.h
+++ abstract-machine/am/src/platform/nemu/include/nemu.h
@@ -22,8 +22,8 @@
 # define DEVICE_BASE 0xa0000000
 #endif
 
-#define MMIO_BASE 0xa0000000
-
+#define MMIO_BASE 0x80000000
+//specialmark
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
 #define KBD_ADDR        (DEVICE_BASE + 0x0000060)
 #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
