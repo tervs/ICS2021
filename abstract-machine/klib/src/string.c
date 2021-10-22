8d8ec3c199212ffb51d088348ff5619ae20ed42c
tracer-ics2021 <tracer@njuics.org>
2021-10-22 00:30:57 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 8ff287da6f6f4a84840b129334317e3c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  00:30:57 up  1:29,  1 user,  load average: 0.83, 1.27, 1.16

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -69,7 +69,13 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  
+  char *p=s;
+  while(n--){*p++=c;}
+  
+  return s;
+  
+  //panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
