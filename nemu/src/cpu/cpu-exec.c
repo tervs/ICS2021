31e1c490c419ae0d522df01c8ec6b02ebdcb7858
tracer-ics2021 <tracer@njuics.org>
2021-10-22 10:01:31 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: c7f0d4ae3e3a451f98c8169f15ec5d98   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  10:01:32 up  2:25,  1 user,  load average: 0.84, 0.75, 0.73

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -114,8 +114,9 @@ void fetch_decode(Decode *s, vaddr_t pc)
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
+  printf("%s\n",p);
   p += space_len;
-  printf("str:%s  size:%ld  pc:0x%08x  code:%hhn  nbyte:%d\n",p, s->logbuf + sizeof(s->logbuf) - p, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
+  //printf("str:%s  size:%ld  pc:0x%08x  code:%hhn  nbyte:%d\n",p, s->logbuf + sizeof(s->logbuf) - p, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.instr.val, ilen);
 #endif
