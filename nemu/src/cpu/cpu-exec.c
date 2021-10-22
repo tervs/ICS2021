0c5f524a58c38f9bcc81afc551b00e142252ccaa
tracer-ics2021 <tracer@njuics.org>
2021-10-22 08:50:34 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: c7f0d4ae3e3a451f98c8169f15ec5d98   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  08:50:34 up  1:14,  1 user,  load average: 1.14, 0.96, 0.82

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,7 +29,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
 {
  // printf("test\n");
 #ifdef CONFIG_ITRACE_COND
-  if (ITRACE_COND) log_write("%s", _this->logbuf);
+  if (ITRACE_COND) log_write("%s\n", _this->logbuf);
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
