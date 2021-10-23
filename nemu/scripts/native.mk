d35984b9245ba99dc9581ec51d18d56de6f45a6c
tracer-ics2021 <tracer@njuics.org>
2021-10-23 03:05:10 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: b5173caf46b04190b60be51854f90d60   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  03:05:10 up  3:43,  1 user,  load average: 0.76, 0.75, 0.81

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -14,7 +14,7 @@ export MPATH=/home/yu/ics2021/am-kernels/tests/cpu-tests/build##
 ALL = $(basename $(notdir $(shell find $(MPATH)/. -name "*-mtrace.txt")))##
 $(ALL): %:%.txt
 %.txt:
-	$(override TEST ?=  --mtrace=$(MPATH)/$*.txt(##
+	$(override TEST ?=  --mtrace=$(MPATH)/$*.txt)
 # Some convenient rules
 
 
