0dc02de61cf1d27cc353d4f39f98ae82205871fe
tracer-ics2021 <tracer@njuics.org>
2021-10-26 00:14:14 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 07ec61b2384e4e98a233454c02cd5bb7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  00:14:14 up  2:15,  1 user,  load average: 1.13, 0.87, 0.92

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -33,7 +33,7 @@ int    atoi      (const char *nptr);
 
 // stdio.h
 int    printf    (const char *format, ...);
-int    mysprintf   (char *str, const char *format, ...);
+int    sprintf   (char *str, const char *format, ...);
 int    snprintf  (char *str, size_t size, const char *format, ...);
 int    vsprintf  (char *str, const char *format, va_list ap);
 int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
