25b52a38a1eae498ba71d60807e61f0fc8e59480
tracer-ics2021 <tracer@njuics.org>
2021-10-25 02:02:20 -0400

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 8ac858f179a44d8892f675829b7b3dab   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  02:02:20 up  1:55,  1 user,  load average: 1.04, 0.70, 0.58

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,9 +4,35 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#define BUFFER_SIZE (400)
+
+/*
+static int isdigit(char c)
+{
+  return '0'<=c&&c<='9';
+}
+
+static int isletter(char c)
+{
+  return 'a'<=c&&c<='z';
+}
+
+*/
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  static char printf_buffer[2*BUFFER_SIZE];
+  va_list arg;
+  va_start(arg,fmt);
+  int ret =vsnprintf(printf_buffer,2*BUFFER_SIZE,fmt,arg);
+  va_end(arg);
+  char *t=printf_buffer;
+  while(*t)
+  {
+    putch(*t);
+    t++;
+  }
+  return ret;
+  //panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
