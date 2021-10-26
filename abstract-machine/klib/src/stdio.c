ec054ed4d98f871d4b67d75679dd6383cc3f81d6
tracer-ics2021 <tracer@njuics.org>
2021-10-26 11:59:19 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d4039edae73442168a79bbf607bcaaab   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  11:59:19 up 18 min,  1 user,  load average: 0.80, 1.03, 0.96

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 #define BUFFER_SIZE (400)
 
-
+/*
 static int isdigit(char c)
 {
   return '0'<=c&&c<='9';
@@ -16,6 +16,44 @@ static int isletter(char c)
 {
   return 'a'<=c&&c<='z';
 }
+
+*/
+
+char* my_itoa(char* dest, int n){
+	/*transfer int into char[]*/
+	//int outn = n;
+	if(n == 0) {
+		dest[0] = '0';
+		dest[1] = '\0';
+		return dest;
+	}
+	
+	int len = 0;
+	int neg = 0;
+	if(n < 0) {
+		neg = 1;
+		len ++;
+		n = -n;
+	}
+	int temp = n;
+	while(temp > 0){
+		len ++;
+		temp /= 10;
+	}
+	
+	for(int i = 0; i < len; ++i){
+		int x = n % 10;
+		dest[len - i - 1] = x + '0';
+		//printf("dest[%d] = %c\n", len-i-1,dest[len-i-1]);
+		n /= 10;
+	}
+	if(neg) dest[0] = '-';
+	dest[len] = '\0';
+	//printf("%d -> %s\n",outn,dest);
+	return dest;
+}
+
+
 
 
 
@@ -45,7 +83,7 @@ int myprintf(const char *fmt, ...) {
 
 
 
-
+/*
 int myvsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   
   static char vsn_buffer[BUFFER_SIZE];
@@ -110,6 +148,135 @@ int myvsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
             }
             break;
           }
+
+        case's':
+        {
+          char *sb=va_arg(ap,char*);
+          char *se=sb;
+          while(*se)se++;
+          while(se!=sb)
+          {
+            *(--cp)=*(--se);
+            outWidth++;
+          }
+          break;
+        }
+
+        default:
+        
+        
+        assert(0);
+
+        break;
+      }
+
+
+      if(pre!='\0')
+      {
+        while(outWidth<mnWidth)
+        {
+          *(--cp)=pre;
+          outWidth++;
+        }
+      }
+
+      while(*cp)
+      {
+        *out=*cp;
+        if(++ret==n)break;
+        outp++;
+        cp++;
+      }
+    }
+
+    else{
+      *outp=cur;
+      outp++;
+      if(++ret==n)break;
+    }
+
+    fmt++;
+  }
+*outp='\0';
+return ret;
+  
+  
+  //panic("Not implemented");
+}
+
+
+int myvsprintf(char *out, const char *fmt, va_list ap) {
+  return myvsnprintf(out,-1,fmt,ap);
+
+}
+*/
+int mysprintf(char *out, const char *fmt, ...) {
+  
+  int len = strlen(fmt);
+	int i = 0;
+	int in_size = 0;
+	va_list var_arg;
+	va_start(var_arg, fmt);
+	out[0] = '\0';
+
+	while(i < len){
+		if(fmt[i] == '%'){
+			int in_num = 0;
+			char in_char[50] = "";
+			switch(fmt[i+1]){
+				case 'd'://%d 
+					in_num = va_arg(var_arg,int);
+					my_itoa(in_char,in_num);
+					strcat(out, in_char);
+					in_size += strlen(in_char);
+					break;
+				case 's'://%s
+				 	strcpy(in_char,va_arg(var_arg, char*));
+					strcat(out,in_char);
+					in_size += strlen(in_char);
+					break;
+				default: assert(0);break;//printf("Not implememted argument type!\n");
+			}
+			i += 2;
+		}
+		else {
+			char temp[2];
+			temp[0] = fmt[i];
+			temp[1] = '\0';
+			strcat(out, temp);
+			in_size ++;
+			i ++;
+		}
+	}
+		va_end(var_arg);
+		return in_size;
+}
+  
+  /*
+  va_list arg;
+  va_start(arg,fmt);
+  int ret =myvsprintf(out,fmt,arg);
+  va_end(arg);
+
+  return ret;
+  */
+  //panic("Not implemented");
+
+
+
+
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  panic("Not implemented");
+}
+
+
+
+
+
+#endif
+
+
 /*
         case'u':
         {
@@ -185,91 +352,6 @@ int myvsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         
         }
 */
-        case's':
-        {
-          char *sb=va_arg(ap,char*);
-          char *se=sb;
-          while(*se)se++;
-          while(se!=sb)
-          {
-            *(--cp)=*(--se);
-            outWidth++;
-          }
-          break;
-        }
-
-        default:
-        
-        
-        assert(0);
-
-        break;
-      }
-
-
-      if(pre!='\0')
-      {
-        while(outWidth<mnWidth)
-        {
-          *(--cp)=pre;
-          outWidth++;
-        }
-      }
-
-      while(*cp)
-      {
-        *out=*cp;
-        if(++ret==n)break;
-        outp++;
-        cp++;
-      }
-    }
-
-    else{
-      *outp=cur;
-      outp++;
-      if(++ret==n)break;
-    }
-
-    fmt++;
-  }
-*outp='\0';
-return ret;
-  
-  
-  //panic("Not implemented");
-}
-
-
-int myvsprintf(char *out, const char *fmt, va_list ap) {
-  return myvsnprintf(out,-1,fmt,ap);
-
-}
-
-int mysprintf(char *out, const char *fmt, ...) {
-  va_list arg;
-  va_start(arg,fmt);
-  int ret =myvsprintf(out,fmt,arg);
-  va_end(arg);
-
-  return ret;
-  
-  //panic("Not implemented");
-}
-
-
-
-/*
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-*/
-
-
-
-
-#endif
-
 
 
 
