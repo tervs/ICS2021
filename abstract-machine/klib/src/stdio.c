723a1f8c4e4e5eb166a73d2fcd40eb11bac33bd9
tracer-ics2021 <tracer@njuics.org>
2021-10-26 17:44:50 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: ec424702016a41648026abe757890cda   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  17:44:50 up  1:33,  1 user,  load average: 1.05, 0.99, 0.83

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -40,12 +40,65 @@ char* my_itoa(char* dest, int n){
 
 
 
-int printf(const char *fmt, ...) {
-  
- 
+int printf(const char *format, ...) {
+	int len = strlen(format);
+	int i = 0;
+	int in_size = 0;
+	va_list var_arg;
+	va_start(var_arg, format);
+
+	while(i < len){
+		if(format[i] == '%'){
+			int in_num = 0;
+			char in_char[50] = "";
+			switch(format[i+1]){
+				case 'd'://%d 
+					in_num = va_arg(var_arg,int);
+					my_itoa(in_char,in_num);
+					//strcat(str, in_char);
+					putstr(in_char);
+					in_size += strlen(in_char);
+					i += 2;
+					break;
+				case 's'://%s
+				 	strcpy(in_char,va_arg(var_arg, char*));
+					//strcat(str,in_char);
+					putstr(in_char);
+					in_size += strlen(in_char);
+					i += 2;
+					break;
+				case '0'://%02d
+					in_num = va_arg(var_arg,int);
+					my_itoa(in_char, in_num);
+					int pwidth = format[i+2] - '0';//width of number
+					for(int i = strlen(in_char); i < pwidth; ++i) {
+						in_size ++;
+						putch('0');
+					}
+					putstr(in_char);
+					in_size += strlen(in_char);
+					i += 4;
+					break;
+
+				default: printf("Not implememted argument type!\n");
+			}
+		}
+		else {
+			//char temp[2];
+			//temp[0] = format[i];
+			//temp[1] = '\0';
+			//strcat(str, temp);
+			putch(format[i]);
+			in_size ++;
+			i ++;
+		}
+	}
+		va_end(var_arg);
+		return in_size;
+}
+int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
-
 
 
 
