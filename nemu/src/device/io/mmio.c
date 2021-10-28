9c2fbfdeea7cae1a0391d1604a61d26f0d40f8d8
201240077-Yu Qiang <201240077@smail.nju.edu.cn>
2021-10-28 21:42:32 +0800

    add many interpretion about map.c and mmio.c

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -1,31 +1,55 @@
+//此文件完成内存映射.属于是中介,或者叫做翻译
+//具体与真机的交互由map.c完成
 #include <device/map.h>
 
 #define NR_MAP 16
 
-static IOMap maps[NR_MAP] = {};
+static IOMap maps[NR_MAP] = {};//定义映射列表
 static int nr_map = 0;
 
-static IOMap* fetch_mmio_map(paddr_t addr) {
-  int mapid = find_mapid_by_addr(maps, nr_map, addr);
-  return (mapid == -1 ? NULL : &maps[mapid]);
+static IOMap* fetch_mmio_map(paddr_t addr) 
+{
+  int mapid = find_mapid_by_addr(maps, nr_map, addr);//函数是字面意思
+  return (mapid == -1 ? NULL : &maps[mapid]);//返回指向结构体的指针.
 }
+//根据虚拟地址,返回对应字段的设备(的结构体指针)
+
 
 /* device interface */
-void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) {
+void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) 
+{
   assert(nr_map < NR_MAP);
-  maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
-    .space = space, .callback = callback };
-  Log("Add mmio map '%s' at [" FMT_PADDR ", " FMT_PADDR "]",
-      maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);
-
+  maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1, .space = space, .callback = callback };
+  Log("Add mmio map '%s' at [" FMT_PADDR ", " FMT_PADDR "]", maps[nr_map].name, maps[nr_map].low, maps[nr_map].high);
   nr_map ++;
 }
+//将物理地址映射到指定的nemu字段(虚拟字段),并且将已经初始化的设备加入maps数组,给定一个编号.
+//IOMAPS 结构的maps记录了每个设备映射的具体情况,space指向到了真机空间.
+
+/*
+
+typedef struct {
+  const char *name;
+  // we treat ioaddr_t as paddr_t here
+  paddr_t low;
+  paddr_t high;
+  void *space;//这里的space指定到了真机地址.工具人而已hhhh
+  io_callback_t callback;
+} IOMap;
+
+*/
+
+
 
 /* bus interface */
-word_t mmio_read(paddr_t addr, int len) {
+word_t mmio_read(paddr_t addr, int len)
+{
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
-void mmio_write(paddr_t addr, int len, word_t data) {
+void mmio_write(paddr_t addr, int len, word_t data) 
+{
   map_write(addr, len, data, fetch_mmio_map(addr));
 }
+//ybb
+//实际上调用了map函数.
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -4,7 +4,8 @@
 
 static uint32_t *rtc_port_base = NULL;
 
-static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
+static void rtc_io_handler(uint32_t offset, int len, bool is_write) 
+{
   assert(offset == 0 || offset == 4);
   if (!is_write && offset == 4) {
     uint64_t us = get_time();
@@ -14,20 +15,25 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
 }
 
 #ifndef CONFIG_TARGET_AM
-static void timer_intr() {
+static void timer_intr() 
+{
   if (nemu_state.state == NEMU_RUNNING) {
     extern void dev_raise_intr();
-    dev_raise_intr();
+    dev_raise_intr();//此函数无定义
   }
 }
 #endif
 
-void init_timer() {
+void init_timer() 
+{
   rtc_port_base = (uint32_t *)new_space(8);
+  //得到了该设备空间的初始地址.是真机的地址.
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
 #else
-  add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);
-#endif
+  add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);//将该设备加入映射中, 即maps数组.
+ //调用rtc_io_handler.实际上初次调用,初始化而已,整个handler结构都没有被调用,仅仅是将函数附加到结构体中.以后将在第一次读写的时候初次赋值
+#endif                                         
   IFNDEF(CONFIG_TARGET_AM, add_alarm_handle(timer_intr));
+  //但是最后调用的这个是啥玩意?串口里可没有这个东西.
 }
