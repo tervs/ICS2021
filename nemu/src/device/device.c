13d36ae46243620b688137e85efecc6a326efa30
tracer-ics2021 <tracer@njuics.org>
2021-10-28 20:29:12 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 2b58aff1f4ae4098b2398b6cca73d413   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:29:12 up  2:33,  1 user,  load average: 1.01, 0.80, 0.66

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -61,7 +61,7 @@ void sdl_clear_event_queue() {
 void init_device() {
   IFDEF(CONFIG_TARGET_AM, ioe_init());
   init_map();
-
+  printf("test\n");
   IFDEF(CONFIG_HAS_SERIAL, init_serial());
   IFDEF(CONFIG_HAS_TIMER, init_timer());
   IFDEF(CONFIG_HAS_VGA, init_vga());
