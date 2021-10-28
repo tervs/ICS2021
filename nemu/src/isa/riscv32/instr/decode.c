57319932b8a3abbc0266b9987e3c8c144e654739
tracer-ics2021 <tracer@njuics.org>
2021-10-28 08:01:37 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 599dffd551ba4b87b8b10dc0a35cbc9f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  08:01:37 up 11 min,  1 user,  load average: 1.82, 1.00, 0.55

--- nemu/src/isa/riscv32/instr/decode.c
+++ nemu/src/isa/riscv32/instr/decode.c
@@ -58,7 +58,7 @@ static def_DHelper(R) {
 static def_DHelper(B) {
   decode_op_r(s, id_src1, s->isa.instr.s.rs1, false);
   word_t offset = (s->isa.instr.b.imm12 << 31) | (s->isa.instr.b.imm12 << 12) | (s->isa.instr.b.imm11 << 11) |(s->isa.instr.b.imm10_5 << 5) |(s->isa.instr.b.imm4_1 << 1) ;
-  printf("offset is 0x%08x\n",s->isa.instr.b.imm12 << 31);
+  printf("offset is 0x%08x\n",offset);//s->isa.instr.b.imm12 << 31);
   decode_op_i(s, id_src2, offset, false);
   decode_op_r(s, id_dest, s->isa.instr.s.rs2, false);
 }
