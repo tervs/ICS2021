364df1721eedf12bb9eb10fe848f575643c3b130
tracer-ics2021 <tracer@njuics.org>
2021-10-29 11:59:30 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 66dcedd2c5e84dabb10b94dfd0a2b116   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  11:59:30 up 15 min,  1 user,  load average: 0.67, 0.64, 0.43

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 
 
+
+static uint64_t read_time() {
+  uint32_t lo = *(volatile uint32_t *)(RTC_ADDR + 0);
+  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
+  uint64_t time = ((uint64_t)hi << 32) | lo;
+  return time / 10;
+}
+
+
+
 //uint32_t start_low;
 //uint32_t start_high;
 uint64_t start;
@@ -10,14 +20,14 @@ void __am_timer_init()
 {
   //start_low=inl(RTC_ADDR);
   //start_high=inl(RTC_ADDR+4);
-  start=inll(RTC_ADDR);
+  start=read_time();
 
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) 
 {
 
-  volatile uint64_t now=inll(RTC_ADDR);
+  //volatile uint64_t now=inll(RTC_ADDR);
 
   //uint32_t low=inl(RTC_ADDR)-start_low;
   //printf("low  %d\n",low);
@@ -25,7 +35,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime)
   //printf("high  %d\n",high);
   //uptime->us = low+(((uint64_t)high)<<32);
 
-  uptime->us=now;
+  uptime->us=read_time()-start;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
