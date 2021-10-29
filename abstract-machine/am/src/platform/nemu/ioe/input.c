c8fcf395fd3177a6e9b1b59525251122a8ed8df8
tracer-ics2021 <tracer@njuics.org>
2021-10-29 14:35:26 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 66dcedd2c5e84dabb10b94dfd0a2b116   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  14:35:26 up  2:50,  1 user,  load average: 0.51, 0.62, 0.66

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,14 +2,14 @@
 #include <nemu.h>
 #include <stdio.h>
 #define KEYDOWN_MASK 0x8000
-//#define KEYDOWN_MASK 0x8000
+
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  //printf("test\n");
+
   uint32_t keyinfo=inl(KBD_ADDR);
   
-        kbd->keycode = keyinfo & (~KEYDOWN_MASK);
-      kbd->keydown = (keyinfo & KEYDOWN_MASK) > 0;
+  kbd->keycode = keyinfo & (~KEYDOWN_MASK);
+  kbd->keydown = (keyinfo & KEYDOWN_MASK) > 0;
   
   /*
   bool s=keyinfo&KEYDOWN_MASK;
@@ -27,10 +27,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   else{kbd->keycode=keyinfo-KEYDOWN_MASK;}
 
   //kbd->keycode=keyinfo^((uint32_t)KEYDOWN_MASK);
-  
-  
-  
-  //kbd->keydown = 0;
-  //kbd->keycode = AM_KEY_NONE;
   */
+
+  
 }
