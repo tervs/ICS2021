2eb8da93d576f2190f09ba7705efc1e7e686a38d
tracer-ics2021 <tracer@njuics.org>
2021-10-29 17:21:02 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d2546492b419422897d0e1d6cd44808f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  17:21:02 up  1:06,  1 user,  load average: 0.87, 0.66, 0.69

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -41,8 +41,7 @@ static void init_screen()
   SDL_CreateWindowAndRenderer(
       SCREEN_W * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
       SCREEN_H * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-      
-0, &window, &renderer);
+      0, &window, &renderer);
   SDL_SetWindowTitle(window, title);
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
