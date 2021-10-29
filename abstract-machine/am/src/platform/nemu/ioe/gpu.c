53ab6c31915cd7abc0b5216e6b59eb996ed403c6
tracer-ics2021 <tracer@njuics.org>
2021-10-29 21:25:43 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d2546492b419422897d0e1d6cd44808f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  21:25:43 up  5:10,  1 user,  load average: 0.94, 1.25, 1.16

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -55,7 +55,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl)
     {
         //fb[i*w+j]=pixels;
         
-      outl(FB_ADDR+4*(i*width+j),(uint32_t)(ctl->pixels));
+      outl(FB_ADDR+4*(i*width+j),0x00888888);
       cnt++ ;
     }
     cnt++;
