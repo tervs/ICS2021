e0548ed5386b7fac5cfc9d960d0c29a08e35fb0c
tracer-ics2021 <tracer@njuics.org>
2021-10-29 21:38:31 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d2546492b419422897d0e1d6cd44808f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  21:38:31 up  5:23,  1 user,  load average: 0.66, 0.90, 1.04

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,7 +24,6 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
     .present = true, .has_accel = false,
     .width=inw(VGACTL_ADDR+2),
     .height=inw(VGACTL_ADDR),
-    //.width = 0, .height = 0,
     .vmemsz = 0
   };
 
@@ -34,40 +33,31 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg)
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) 
 {
 
-      int width = io_read(AM_GPU_CONFIG).width;
-      //int height = io_read(AM_GPU_CONFIG).height;
-
-
+    int width = io_read(AM_GPU_CONFIG).width;
     int x=ctl->x;
     int y=ctl->y;
     int w=ctl->w;
     int h=ctl->h;
-    //int block_size = w * h;
     uintptr_t *pixels=ctl->pixels;
 
-    //uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    int cnt=0;
 
-
-  int cnt=0;
-  for(int i=y;i<y+h;i++)
-  {
-    for(int j=x;j<x+w;j++)
+    for(int i=y;i<y+h;i++)
     {
-        //fb[i*w+j]=pixels;
-        
-      outl(FB_ADDR+4*(i*width+j),*(pixels+cnt));
-      //printf("%d\n",*(pixels+4*cnt));
-      cnt++ ;
-    }
-    //cnt++;
-  }
-
-
-
-      if (ctl->sync) 
+      for(int j=x;j<x+w;j++)
       {
-        outl(SYNC_ADDR, 1);
+        outl(FB_ADDR+4*(i*width+j),*(pixels+cnt));
+        printf("%d\n",*(pixels+4*cnt));
+        cnt++ ;
       }
+    }
+
+
+
+    if (ctl->sync) 
+    {
+      outl(SYNC_ADDR, 1);
+    }
 
 }
 
