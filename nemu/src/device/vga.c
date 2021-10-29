393d1e10e602083418df56696b20ebe4e042ebac
tracer-ics2021 <tracer@njuics.org>
2021-10-29 17:30:38 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d2546492b419422897d0e1d6cd44808f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  17:30:38 up  1:15,  1 user,  load average: 0.57, 0.62, 0.64

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -71,11 +71,16 @@ static inline void update_screen() {
 #endif
 #endif
 
+
+
 void vga_update_screen() 
 {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }
+
+
+
 
 void init_vga() 
 {
@@ -90,5 +95,5 @@ void init_vga()
   vmem = new_space(screen_size());
   add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
-  IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0xFFFF0000, screen_size()));
+  IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
 }
