fccad005bb6af179c27d664649b64f21b8c8008c
tracer-ics2021 <tracer@njuics.org>
2021-11-30 02:10:25 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 79804ebc905b4d9099bd6d801434dc38   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  02:10:25 up  2:09,  1 user,  load average: 0.85, 1.27, 0.83

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -18,11 +18,11 @@ static bool g_print_step = false;
 const rtlreg_t rzero = 0;
 rtlreg_t tmp_reg[4];
 //Decode s;
-rtlreg_t csr[4096];
-extern rtlreg_t _mepc;
-extern rtlreg_t _mstatus;
-extern rtlreg_t _mcause;
-extern rtlreg_t _mtvec;
+//rtlreg_t csr[4096];
+rtlreg_t _mepc;
+rtlreg_t _mstatus;
+rtlreg_t _mcause;
+rtlreg_t _mtvec;
 
 void device_update();
 void fetch_decode(Decode *s, vaddr_t pc);
