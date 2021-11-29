a200d9d06636df86baa14e998a7a3cc991db3b0b
tracer-ics2021 <tracer@njuics.org>
2021-11-30 03:13:52 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 79804ebc905b4d9099bd6d801434dc38   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  03:13:52 up  3:12,  1 user,  load average: 0.95, 0.94, 0.85

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -10,7 +10,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t *epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  printf("0x%8u\n\n\n\n",*epc);
+  printf("0x%16u\n\n\n\n",*epc);
     _mepc=*epc;
     _mcause=NO;
     *epc=_mtvec;
