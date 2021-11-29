1f7f71ef9b3322245fd70fd368d974ce87311a67
tracer-ics2021 <tracer@njuics.org>
2021-11-30 03:00:35 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 79804ebc905b4d9099bd6d801434dc38   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  03:00:35 up  2:59,  1 user,  load average: 0.88, 0.95, 0.78

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -1,9 +1,9 @@
 #include <isa.h>
 
 extern rtlreg_t _mepc;
-//rtlreg_t _mstatus=0x1800;
-//rtlreg_t _mcause;
-//rtlreg_t _mtvec;
+extern rtlreg_t _mstatus;
+extern rtlreg_t _mcause;
+extern rtlreg_t _mtvec;
 
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
@@ -11,7 +11,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
     _mepc=epc;
-
+    _mcause=NO;
+    epc=_mtvec;
   return 0;
 }
 
