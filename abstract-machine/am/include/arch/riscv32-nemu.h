f8618d8621e3d3c2e7b1e725535830b6ac42eb6a
tracer-ics2021 <tracer@njuics.org>
2021-12-01 18:27:50 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 597a5f303ce74b209e7cdeedcb19621e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:27:50 up  7:00,  1 user,  load average: 0.82, 0.66, 0.65

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -3,7 +3,7 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t gpr[32], mcause, mepc, mstatus;
+  uintptr_t gpr[32], mepc,mcause,  mstatus;
   void *pdir;
 };
 
