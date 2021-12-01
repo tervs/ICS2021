c3f77466e57dbf5b91eb9943ee04ec850366c1b3
tracer-ics2021 <tracer@njuics.org>
2021-12-01 23:58:11 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 3b19b09abcae4128aed0bec45465cdbc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:58:11 up  1:49,  1 user,  load average: 0.33, 0.58, 0.58

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,18 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
+
+size_t get_ramdisk_size();
+
+
+
 Elf_Ehdr *elf;
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  printf("0x%08x\n",*(uint32_t *)elf->e_ident);
-  assert(*(uint32_t *)elf->e_ident == 0x7f454c47);
+  //printf("0x%08x\n",*(uint32_t *)elf->e_ident);
+  //assert(*(uint32_t *)elf->e_ident == 0x7f454c47);
+
+  uint32_t x=get_ramdisk_size();
+  printf("%d\n",x);
   printf("test2\n");
   TODO();
   return 0;
