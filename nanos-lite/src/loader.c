3682601cd6e87ce532dadeb2f60d05d45bae2c0f
tracer-ics2021 <tracer@njuics.org>
2021-12-02 11:07:54 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1a6711c0c1c4e8b83f26ecbee67dae2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:07:54 up 46 min,  1 user,  load average: 0.36, 0.42, 0.44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <stdio.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,17 +10,32 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 #define ENTRY 0x83000000
-//#define N 100000
+
 size_t get_ramdisk_size();
 size_t ramdisk_read(void *buf, size_t offset, size_t len) ;
 size_t ramdisk_write(const void *buf, size_t offset, size_t len) ;
-//size_t buf[N];
-
-
-
 
 Elf_Ehdr *elf;
+
+
+
+
+ 
+
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
+
+
+  FILE* file = fopen("/build/ramdisk.img", "rb");
+  if(file) {
+    // read the header
+    fread(elf, 1, sizeof(elf), file);
+  }
+
+  printf("0x%08x\n",elf->e_ident);
+
+
   //printf("0x%08x\n",*(uint32_t *)elf->e_ident);
   //assert(*(uint32_t *)elf->e_ident == 0x7f454c47);
   ramdisk_read((void *)ENTRY,0x000000,get_ramdisk_size());
