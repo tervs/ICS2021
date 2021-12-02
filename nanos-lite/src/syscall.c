807926bc8b85a849453955a794c3616845dcc053
tracer-ics2021 <tracer@njuics.org>
2021-12-02 22:06:37 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 1aa41ac94aa54e00b57b096dfe1c583f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  22:06:37 up  3:14,  1 user,  load average: 0.74, 1.01, 0.98

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,13 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  
+
+
+
   //printf("%d\n\n\n\n",a[0]);
   switch (a[0]) {
     case 0: printf("syscall 0\n");halt(0);break;
