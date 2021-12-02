ff728d1f24a74bcf7aeb5a1d7dfd11b53e96cb3c
tracer-ics2021 <tracer@njuics.org>
2021-12-02 13:28:19 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1a6711c0c1c4e8b83f26ecbee67dae2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  13:28:19 up  3:06,  1 user,  load average: 0.77, 0.68, 0.58

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -49,8 +49,8 @@ def_EHelper(jalr) {
 
   rtl_li(s, ddest, s->pc+4);
   rtl_sext(s,&(id_src2->imm),&(id_src2->imm),12);
-  printf("0x%08x\n", (id_src2->imm));
-  rtl_addi(s,dsrc1,dsrc1, ((id_src2->imm)<<20)>>20);
+  //printf("0x%08x\n", (id_src2->imm));
+  rtl_addi(s,dsrc1,dsrc1, (id_src2->imm));
   
 
   rtl_li(s, dsrc1, (*dsrc1>>1)<<1);
