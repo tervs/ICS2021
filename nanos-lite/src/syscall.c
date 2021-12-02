6cc54b473be72a3ffe69008ff13c44562826089d
tracer-ics2021 <tracer@njuics.org>
2021-12-02 23:38:46 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 1aa41ac94aa54e00b57b096dfe1c583f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:38:46 up  4:46,  1 user,  load average: 0.80, 1.55, 1.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ void do_syscall(Context *c) {
 
 
   //printf("%d\n\n\n\n",a[0]);
-  switch (a[0]) {
+  switch (a[1]) {
     case 0: printf("syscall 0\n");halt(0);break;
     case 1: printf("syscall 1\n");yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
