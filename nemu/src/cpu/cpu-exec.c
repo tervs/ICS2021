1e89313f131da80f5654d8b3e92e1a9b332f3fce
tracer-ics2021 <tracer@njuics.org>
2021-12-03 18:24:30 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 24defc9b38f9483893f876a406dcdea7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  18:24:30 up  4:12,  1 user,  load average: 1.15, 0.85, 0.86

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -126,7 +126,7 @@ void cpu_exec(uint64_t n) {
   Decode s;//todo tobe delete
   for (;n > 0; n --) {
     fetch_decode_exec_updatepc(&s);
-    printf(" pc:  0x%08x\n",s.pc);
+    //printf(" pc:  0x%08x\n",s.pc);
     if(cpu.pc==0x830000e4)
     {
       nemu_state.state=NEMU_ABORT;
