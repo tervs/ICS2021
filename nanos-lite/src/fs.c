b21f6cf51096b2235e0f389abdc71baa9baf47b0
tracer-ics2021 <tracer@njuics.org>
2021-12-04 05:17:04 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 60e0d4f3cb4b4045afb7fab68bf77f7b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  05:17:04 up  1:08,  1 user,  load average: 0.41, 0.46, 0.46

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -117,7 +117,7 @@ size_t fs_lseek(int fd, size_t offset, int whence)
   {
 		case SEEK_SET:file_table[fd].open_offset=offset;break;
 		case SEEK_CUR:file_table[fd].open_offset+=offset;break;
-		case SEEK_END:file_table[fd].open_offset=file_table[fd].size+offset;break;
+		case SEEK_END:file_table[fd].open_offset=file_table[fd].size-offset;break;//file_table[fd].open_offset=file_table[fd].size+offset;break;
     default: assert(0);
   }
     
