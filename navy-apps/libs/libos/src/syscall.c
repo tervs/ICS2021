ba68e537f9900bc325f5692a9e2cea5b3a1b684b
tracer-ics2021 <tracer@njuics.org>
2021-12-03 14:54:56 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 24defc9b38f9483893f876a406dcdea7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:54:56 up 42 min,  1 user,  load average: 1.57, 1.34, 1.21

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,7 +63,7 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count) {
   
-  printf("fd  %d   buf  %d   count %d\n",fd, (intptr_t)buf,count);
+  //printf("fd  %d   buf  %d   count %d\n",fd, (intptr_t)buf,count);
   return _syscall_(SYS_write,fd,(intptr_t)buf,count);
  
   //_exit(SYS_write);
