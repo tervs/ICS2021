2cfaff6c26eeb8fc92aadec4e8809f3df10f843c
tracer-ics2021 <tracer@njuics.org>
2021-12-03 16:58:26 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 24defc9b38f9483893f876a406dcdea7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:58:26 up  2:46,  1 user,  load average: 1.67, 1.24, 1.13

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -44,17 +44,16 @@ uintptr_t sys_exit()
 
 uintptr_t sys_write()
 {
-
+if(a[1]==1||a[1]==2)
+{
   printf("syscall 4\n");
   char *ch=(char *)(a[2]);
-  
-  
   for(int i=0;i<a[3];i++)
   {
     putch(*(ch+i));
-    //printf("  %p\n",ch+i);
   }
-  //printf("\n");
-  //printf("a0: %d  a1: %d  a2: 0x%08x  a3: %d\n",a[0],a[1],a[2],a[3]);
+
   return a[3];
+}
+return -1;
 }
