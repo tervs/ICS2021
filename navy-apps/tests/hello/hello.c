195ea777a67ddf47e49ac2ab598fd1b1bd76d2fc
tracer-ics2021 <tracer@njuics.org>
2021-12-03 20:30:56 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 24defc9b38f9483893f876a406dcdea7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:30:57 up  6:18,  1 user,  load average: 0.83, 0.85, 0.88

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,11 +5,11 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (j<1001) {
+  while (1) {
     j ++;
     if (j == 1000) {
       printf("Fello World from Navy-apps for the %dth time!\n", i ++);
-      //j = 0;
+      j = 0;
     }
   }
   return 0;
