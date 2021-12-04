27e779fce97e85bd01b717efdf0a63cdff2b16fc
tracer-ics2021 <tracer@njuics.org>
2021-12-04 20:41:07 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: e26701a31e684ef48661c2ed70f4f7d9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:41:07 up  4:44,  1 user,  load average: 0.42, 0.70, 0.73

--- navy-apps/tests/timer-test/time.c
+++ navy-apps/tests/timer-test/time.c
@@ -4,15 +4,15 @@
 
 int main() 
 {
+    printf("test\n");
     struct timeval *tv;
-
+printf("test\n");
     gettimeofday(tv,NULL);
-    while(1)
-    {
-     printf("tv_sec:%ld\n",(tv->tv_sec));
-     printf("tv_usec:%ld\n",tv->tv_usec);
+    long int x=(*tv).tv_sec;
+printf("%ld\n",x);
+    printf("tv_sec:%ld\n",(*tv).tv_sec);
+     printf("tv_usec:%ld\n",(*tv).tv_usec);
 
 
-    }
   return 0;
 }
