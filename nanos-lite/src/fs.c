99a4152192d907233a39175d5f94b585d6d49d43
tracer-ics2021 <tracer@njuics.org>
2021-12-05 14:54:00 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 77e2fbd27be945e5a8d25939f0371cd7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  14:54:01 up  2:27,  1 user,  load average: 0.55, 0.53, 0.28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -79,7 +79,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
 
 int fs_open(const char *pathname, int flags, int mode)
 {
-  //printf("%s\n",pathname);
+  printf("%s\n",pathname);
   for(int i=0;i<23;i++)
   {
     //printf("%s\n",file_table[i].name);
