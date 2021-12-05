86e094c1462a9997459ab3a96f941fac3a02e783
tracer-ics2021 <tracer@njuics.org>
2021-12-05 12:43:47 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 77e2fbd27be945e5a8d25939f0371cd7   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  12:43:47 up 17 min,  1 user,  load average: 0.57, 0.56, 0.34

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -14,6 +14,11 @@ static const char *keyname[256] __attribute__((used)) = {
   AM_KEYS(NAME)
 };
 
+
+
+
+
+
 size_t serial_write(const void *buf, size_t offset, size_t len) 
 {
   //printf("test\n\n\n\n\n\n\n\n\n");  
@@ -29,6 +34,15 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+
+
+      while (1) 
+    {
+      AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+      if (ev.keycode == AM_KEY_NONE) break;
+      printf("Got  (kbd): %s (%d) %s\n", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
+    }
+  
   return 0;
 }
 
