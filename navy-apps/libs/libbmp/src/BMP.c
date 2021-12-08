b45901e7b258b5fd4c4c208254177fe04bc1a94e
tracer-ics2021 <tracer@njuics.org>
2021-12-09 00:12:24 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d9dd2d3969344637a5753ddfd5ace553   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:12:24 up 33 min,  1 user,  load average: 1.20, 1.08, 0.81

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -36,7 +36,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
 
   int line_off = (w * 3 + 3) & ~0x3;
   for (int i = 0; i < h; i ++) {
-    printf("here?\n");
+    //printf("here?\n");
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
     int nread = fread(&pixels[w * i], 3, w, fp);
     for (int j = w - 1; j >= 0; j --) {
@@ -46,7 +46,7 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
-
+printf("here?\n");
   fclose(fp);
   if (width) *width = w;
   if (height) *height = h;
