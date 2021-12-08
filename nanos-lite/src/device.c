9a8f9503dd9f39852bc1937d928364e619369d1d
tracer-ics2021 <tracer@njuics.org>
2021-12-09 00:16:40 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d9dd2d3969344637a5753ddfd5ace553   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  00:16:40 up 37 min,  1 user,  load average: 1.13, 1.09, 0.88

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -59,7 +59,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   printf("just for test\n");
   int h = io_read(AM_GPU_CONFIG).height;
   uint64_t w = (uint64_t)io_read(AM_GPU_CONFIG).width;
-  uint64_t src=w<<32||h;
+  uint64_t src=w<<32|h;
   printf("%d  %d  %d\n",h,w,src);
   /*
   //uint64_t w = (uint64_t)io_read(AM_GPU_CONFIG).width;
