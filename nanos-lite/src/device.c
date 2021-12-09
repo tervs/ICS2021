b76dd6cb26b8b585db6e17fa0f5b36a3bc1c06d4
tracer-ics2021 <tracer@njuics.org>
2021-12-09 17:44:36 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: a49bd3b7194a4b208ffa952c0276ef62   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:44:36 up  1:36,  1 user,  load average: 0.54, 0.53, 0.54

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -17,7 +17,7 @@ static const char *keyname[256] __attribute__((used)) = {
 
   extern int screen_w;
   extern int screen_h;
-
+static int i=0;
 
 
 size_t serial_write(const void *buf, size_t offset, size_t len) 
@@ -90,8 +90,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 
   uint32_t *pixels= (uint32_t *)buf;
   
-  io_write(AM_GPU_FBDRAW, 32, 32,pixels, len, 1, true);
-  
+  io_write(AM_GPU_FBDRAW, 32, i,pixels, len, 1, true);
+  i++;
   return 0;
 }
 
