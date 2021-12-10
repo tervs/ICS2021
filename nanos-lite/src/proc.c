a3d9eabe756953157c150ae8520891eb781ccae5
tracer-ics2021 <tracer@njuics.org>
2021-12-10 10:58:18 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: ca5883b7b9e54014962d410d3c2c78fd   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  10:58:18 up  2:16,  1 user,  load average: 1.22, 0.77, 0.54

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   switch_boot_pcb();
   
   Log("Initializing processes...");
-  naive_uload(NULL,"/bin/bmp-test");
+  naive_uload(NULL,"/bin/nslider");
   Log("test");
   // load program here
 
