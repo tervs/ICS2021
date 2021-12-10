7f3695d7ae7994a2c656cca420c19bd261c3c293
tracer-ics2021 <tracer@njuics.org>
2021-12-10 17:01:32 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: ca5883b7b9e54014962d410d3c2c78fd   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  17:01:33 up  8:20,  1 user,  load average: 0.73, 0.74, 0.62

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -27,6 +27,9 @@ int SDL_WaitEvent(SDL_Event *event) {
   if (NDL_PollEvent(buf, sizeof(buf)))
   {
     printf("waitevent %s\n",buf);
+    char *keycode=strtok(buf," ");
+    char *type=strtok(NULL,"\0");
+
     //printf("%c \n",buf[0]);
     /*
     for(int i=0;i<64;i++)
