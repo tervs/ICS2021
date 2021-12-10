2468d2910091c0952db5c2a1f92fa6638b9aacb8
tracer-ics2021 <tracer@njuics.org>
2021-12-10 11:22:04 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: ca5883b7b9e54014962d410d3c2c78fd   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  11:22:04 up  2:40,  1 user,  load average: 0.45, 0.73, 0.69

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -42,6 +42,7 @@ SDL_Surface* SDL_CreateRGBSurface(uint32_t flags, int width, int height, int dep
   assert(s);
   s->flags = flags;
   s->format = malloc(sizeof(SDL_PixelFormat));
+  printf("format 0x%08x\n",(uint32_t)(s->format));
   assert(s->format);
   if (depth == 8) {
     s->format->palette = malloc(sizeof(SDL_Palette));
@@ -85,7 +86,7 @@ SDL_Surface* SDL_CreateRGBSurfaceFrom(void *pixels, int width, int height, int d
 
 void SDL_FreeSurface(SDL_Surface *s) {
   if (s != NULL) 
-  { printf("format %p\n",s->format);
+  { //printf("format %p\n",s->format);
     printf("1\n");
     if (s->format != NULL) 
     {printf("2\n");
