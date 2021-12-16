c77723575ab047e52e128e70c4a89ba8a7aa24eb
tracer-ics2021 <tracer@njuics.org>
2021-12-16 19:11:51 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: dd629c498e96475581bcae046d668edc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  19:11:51 up  3:11,  1 user,  load average: 0.51, 0.54, 0.74

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -36,7 +36,7 @@ void render() {
   
   //sprintf(fname, path, 0);
   slide = SDL_LoadBMP(fname);
-  printf("cur 0x%08x\n",(uint32_t)(&cur));
+  //printf("cur 0x%08x\n",(uint32_t)(&cur));
   printf("slide w %d slide h %d \n",slide->w,slide->h);
   assert(slide);
   SDL_UpdateRect(slide, 0, 0, 0, 0);
