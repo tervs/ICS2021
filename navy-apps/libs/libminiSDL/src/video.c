4b4d46d57ea21422bc15d802294634d41bfcd939
tracer-ics2021 <tracer@njuics.org>
2021-12-16 20:21:55 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: dd629c498e96475581bcae046d668edc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  20:21:55 up  4:22,  1 user,  load average: 0.53, 0.58, 0.42

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -11,6 +11,11 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
+uint32_t *x=malloc((dstrect->h)*(dstrect->w));
+memset(x,color,(dstrect->h)*(dstrect->w));
+  NDL_DrawRect(x,dstrect->x,dstrect->y,dstrect->w,dstrect->h);
+  //SDL_UpdateRect(dst,)
+
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
