0759ecfc53a76d70898818472f1ece0fd3825a6e
tracer-ics2021 <tracer@njuics.org>
2021-12-16 23:32:07 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: dd629c498e96475581bcae046d668edc   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  23:32:07 up  7:32,  1 user,  load average: 1.09, 0.72, 0.82

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -6,9 +6,11 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
+  printf("mem read in 0x%08x  len 0x%08x\n",addr,len);
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
+  printf("mem write in 0x%08x  len 0x%08x   data 0x%08x\n",addr,len,data);
   paddr_write(addr, len, data);
 }
