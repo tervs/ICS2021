e7b7a4daac0c5168f3e3cf209a661b81ab7aeb06
tracer-ics2021 <tracer@njuics.org>
2022-01-11 23:24:04 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  23:24:04 up 1 day,  6:19,  1 user,  load average: 0.69, 0.78, 0.60

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -14,7 +14,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   
   //printf("is  %p\n\n\n",dstrect);
   printf("w %d   h %d\n",dst->w,dst->h);
-  printf("x  %d  y %d\n",dstrect->x,dstrect->y);
+  printf("x  %d  y %d\n",dstrect->w,dstrect->h);
   uint32_t *x=malloc(400*300);
   //printf("x is %p\n",&color);
   //printf("x is %p\n",&color);
