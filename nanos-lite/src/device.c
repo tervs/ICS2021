e2e5906e39323dc479363f2340f2a19356d19f74
tracer-ics2021 <tracer@njuics.org>
2022-01-11 20:25:30 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  20:25:30 up 1 day,  3:20,  1 user,  load average: 1.33, 0.63, 0.37

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,14 +23,7 @@ static const char *keyname[256] __attribute__((used)) = {
   [AM_KEY_NONE] = "NONE",
   AM_KEYS(NAME)
 };
-/*
-size_t serial_write(const void *buf, size_t offset, size_t len) {
-  for(int i = 0; i < len;i ++) {
-      putch(((char*)buf)[i]);
-    }
-  return len;
-}
-*/
+
 size_t serial_write(const void *buf, size_t offset, size_t len) 
 {
   //printf("test\n\n\n\n\n\n\n\n\n");  
@@ -44,26 +37,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len)
   
   //return len;
 }
-/*
-size_t events_read(void *buf, size_t offset, size_t len) {
-  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if(ev.keycode == AM_KEY_NONE) {
-    //memset(buf,0,len);
-    return 0;
-  }
-  else{
-    memset(buf,0,len);
-    if(ev.keydown)
-    {
-      sprintf((char*)buf,"kd %s\n",keyname[ev.keycode]);
-    }
-    else sprintf((char*)buf,"ku %s\n",keyname[ev.keycode]);
-    //printf("%s %d",(char*)buf,strlen((char*)buf));
-    return strlen((char*)buf);
 
-  }
-}
-*/
 size_t events_read(void *buf, size_t offset, size_t len) {
 
 //printf("test\n");
@@ -72,7 +46,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
       else
       {
           //printf("")
-         snprintf(buf,len,"%s %s\n",keyname[ev.keycode],(ev.keydown ? "DOWN" : "UP"));
+         snprintf(buf,len,"%s %s\n",(ev.keydown ? "kd" : "ku"),keyname[ev.keycode]);
           //strcpy(buf,keyname[ev.keycode]);
           //strcat(buf,(ev.keydown ? "DOWN" : "UP"));
           //printf("event test %s\n",buf);
@@ -85,13 +59,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   
   return -1;
 }
-/*
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
-  return sprintf((char*)buf,"%d:%d\n",w,h);
-}
-*/
+
+
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   //printf("0x%08x\n",(uint32_t)(buf));
@@ -109,18 +78,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   //return 0;
 }
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  /*
-  int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
-  int x = (offset/4)%w;
-  int y = (offset/4)/w;
-  if(offset+len > w*h*4) len = w*h*4 - offset;
-
-  io_write(AM_GPU_FBDRAW,x,y,(uint32_t*)buf,len/4,1,false);
-  //io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-  assert(offset <= w*h*4);
-  return len;
-  */
 
 
     uint32_t *pixels= (uint32_t *)buf;
@@ -139,17 +96,6 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 //在这里处理超出的字符
 
-/*
-size_t fb_sync(const void *buf,size_t offset,size_t len)
-{
-  printf("call sync");
-  assert(((char*)buf)[0] == '1');
-  //printf("refresh\n");
-  assert(offset == 0);
-  io_write(AM_GPU_FBDRAW, 0, 0, NULL, 0, 0, true);
-  return 1;
-}
-*/
 
 
 void init_device() {
