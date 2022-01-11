81d5ba6e66161e6f3e6b807000eac41b1bd8997a
tracer-ics2021 <tracer@njuics.org>
2022-01-11 21:16:02 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:16:02 up 1 day,  4:11,  1 user,  load average: 1.16, 0.97, 0.74

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -70,7 +70,7 @@ int SDL_WaitEvent(SDL_Event *event) {
     char* str = key+3;
     for(int i = 0,cnt = 0;i < sizeof(keyname);i+=sizeof(keyname[cnt]),cnt+=1)
     {
-      printf("cnt %d  keyname[cnt] %s   str  %s\n",cnt,keyname[cnt],str);
+      printf("cnt %d  keyname[cnt] :%s:   str  :%s:\n",cnt,keyname[cnt],str);
       printf("%d    %d   %d\n",strncmp(keyname[cnt],str,strlen(str)),strncmp(keyname[cnt],str,strlen(keyname[cnt])),strcmp(keyname[cnt],str));
     //if(!strncmp(keyname[cnt],str,strlen(str))&&!strncmp(keyname[cnt],str,strlen(keyname[cnt]))) {
         if(!strcmp(keyname[cnt],str))
