7e9276d4098f3f59b5ad47d3fa9fbf4878e09308
tracer-ics2021 <tracer@njuics.org>
2022-01-12 17:48:27 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:48:27 up 2 days, 43 min,  1 user,  load average: 1.65, 1.38, 0.96

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -72,6 +72,10 @@ static void clear_display(void) {
 int main(int argc, char *argv[], char *envp[]) {
   SDL_Init(0);
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
+    while(1)
+  {
+    printf("test\n");
+  }
   //printf("in test %d  %d\n",screen->w,screen->h);
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
