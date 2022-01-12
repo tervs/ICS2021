fdeccba9903f9f96913b3fffab5a964653372cb2
tracer-ics2021 <tracer@njuics.org>
2022-01-12 21:27:32 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:27:32 up 2 days,  4:22,  1 user,  load average: 1.49, 0.94, 0.86

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -45,7 +45,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 
   uint32_t *src_color=(uint32_t *)(src->pixels);
   uint32_t *dst_color=(uint32_t *)(dst->pixels);
-  printf("x:%d  y:%d  w:%d  h:%d   in bilt\n",dst_x,dst_y,src_w,src_h);
+  //printf("x:%d  y:%d  w:%d  h:%d   in bilt\n",dst_x,dst_y,src_w,src_h);
   //uint8_t* dst_color = dst->pixels,*src_color = src->pixels;
   //uint32_t color_width = dst->format->palette?1:4;
   uint32_t colorbit = (dst->format->palette!=NULL)? 1 : 4;
