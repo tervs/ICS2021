9eb6f018cac4f857ca5628b5e197a842ba1b61ac
tracer-ics2021 <tracer@njuics.org>
2022-01-12 21:31:19 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:31:19 up 2 days,  4:26,  1 user,  load average: 1.37, 1.06, 0.92

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -117,9 +117,9 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
 
-printf("test in update\n");
+//printf("test in update\n");
 uint32_t *p=(uint32_t *)(s->pixels);
-  printf("x:%d y:%d w:%d h:%d in updaterect\n",x,y,w,h);
+  //printf("x:%d y:%d w:%d h:%d in updaterect\n",x,y,w,h);
   NDL_DrawRect(p, x,  y,  w,  h); 
 
 //while(1);
