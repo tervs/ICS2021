6e761e64457bbf4aaeb123b5071fe4ee1abe07ba
tracer-ics2021 <tracer@njuics.org>
2022-01-12 21:07:38 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  21:07:38 up 2 days,  4:02,  1 user,  load average: 0.57, 0.79, 0.82

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -47,10 +47,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   uint32_t *dst_color=(uint32_t *)(dst->pixels);
   printf("x:%d  y:%d  w:%d  h:%d   in bilt\n",dst_x,dst_y,src_w,src_h);
   //uint8_t* dst_color = dst->pixels,*src_color = src->pixels;
-  uint32_t color_width = dst->format->palette?1:4;
+  //uint32_t color_width = dst->format->palette?1:4;
   
     for(int i = 0;i < src_h;i++)
-    memcpy(dst_color+color_width*((i+dst_y)*dst->w+dst_x),src_color+color_width*((i+src_y)*src->w+src_x),color_width*src_w);
+    memcpy(dst_color+((i+dst_y)*dst->w+dst_x),src_color+((i+src_y)*src->w+src_x),src_w);
   //NDL_DrawRect(p,dst_x,dst_y,src_w,src_h);
   
   //printf("color width %d\n",color_width);
