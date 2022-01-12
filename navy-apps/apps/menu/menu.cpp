73618b37bef7b5816a43c798e4bb706d41f59ab6
tracer-ics2021 <tracer@njuics.org>
2022-01-12 17:32:09 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  17:32:09 up 2 days, 27 min,  1 user,  load average: 1.66, 0.81, 0.41

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -66,7 +66,7 @@ static void prev() {
 }
 
 static void clear_display(void) {
-  SDL_FillRect(screen, NULL, 0x888888);
+  SDL_FillRect(screen, NULL, 0xffffff);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
