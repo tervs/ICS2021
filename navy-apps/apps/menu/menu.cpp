4317aa7cb87f0ab284a6f78780e99d4369223356
tracer-ics2021 <tracer@njuics.org>
2022-01-12 20:50:39 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  20:50:39 up 2 days,  3:45,  1 user,  load average: 0.59, 0.57, 0.64

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -150,7 +150,7 @@ static void draw_text_row(char *s, int r) {
 
 static void display_menu(int n) {
   printf("kjkjlogo %d\n",logo_sf->w);
-  clear_display();
+  //clear_display();
  printf("logo %d\n",logo_sf->w);
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   //printf("dstretc  x%d   \n",rect.x);
