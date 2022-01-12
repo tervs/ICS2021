355cffa9d129c404549da86006dd462a9847bef6
tracer-ics2021 <tracer@njuics.org>
2022-01-12 19:31:13 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d1930f63407d4a99bb2afe2079ed02fb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-44-generic       Architecture: x86-64  19:31:13 up 2 days,  2:26,  1 user,  load average: 0.66, 0.79, 0.62

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -151,9 +151,10 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
    h=screen_h;
    //lseek(DEV_FB,0,SEEK_SET);
  }
+ printf("x:%d  y:%d in drawrect\n",x,y);
   x=center_x+x;
   y=center_y+y;
-  printf("x:%d  y:%d in drawrect\n",x,y);
+  
   lseek(DEV_FB,y*screen_w+x,SEEK_SET);
   for(int j=y;j<y+h;j++)
   {
