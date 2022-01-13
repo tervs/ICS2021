e834d864e0af432652a3a80f0726bc41de7eade3
tracer-ics2021 <tracer@njuics.org>
2022-01-14 01:41:30 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  01:41:30 up 1 day,  1:45,  1 user,  load average: 1.31, 1.19, 1.12

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -60,7 +60,7 @@ int SDL_PollEvent(SDL_Event *ev) {
     ev->type = SDL_KEYDOWN;
     keystate[keycode] = 1;
   }
-    printf("keycode %s\n",keyname[keycode]);
+    //printf("keycode %s\n",keyname[keycode]);
     return 1;
   }
   else 
