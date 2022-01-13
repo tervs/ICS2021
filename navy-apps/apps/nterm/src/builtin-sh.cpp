6b60aa588fe457680fdce3a4d750e74516a7429d
tracer-ics2021 <tracer@njuics.org>
2022-01-13 22:20:41 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  22:20:41 up 22:24,  1 user,  load average: 0.94, 0.84, 0.84

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -26,8 +26,8 @@ static void sh_handle_cmd(const char *cmd) {
   char *s;
   strcpy(s,cmd);
   //s=strtok(s,"\r");
-  printf("cmd %s",cmd);
-  printf("s %s\n",s);
+  //printf("cmd %s",cmd);
+  //printf("s %s\n",s);
   execve(cmd,0,0);
   
 }
