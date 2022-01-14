e73cbf2f4abaefbbdae2d7d1332048edc54805b8
tracer-ics2021 <tracer@njuics.org>
2022-01-14 21:04:19 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  21:04:19 up 1 day, 21:07,  1 user,  load average: 1.47, 0.94, 0.74

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -26,6 +26,7 @@ Context* __am_irq_handle(Context *c) {
     }
   //printf("begin excute\n");
     c = user_handler(ev, c);
+    printf("c addr %x\n",(uintptr_t)c);
     /*
     uint32_t cp=(uint32_t )c;
     uint32_t x0=0;
@@ -35,8 +36,7 @@ Context* __am_irq_handle(Context *c) {
     :
     :[src]"r"(cp),[src2]"r"(x0)
     );
-    */
-    /*
+    
     printf("c addr %x\n",(uintptr_t)c);
     uint32_t *pt=(uint32_t *)(uintptr_t)c;
     
