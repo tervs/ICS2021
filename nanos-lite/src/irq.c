001e7a5181093d7f09bc24777615855fbe52bc3d
tracer-ics2021 <tracer@njuics.org>
2022-01-14 18:30:05 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  18:30:05 up 1 day, 18:33,  1 user,  load average: 0.76, 0.46, 0.27

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,13 @@
 
 
 #include <common.h>
-
+Context* schedule(Context *prev);
 
 int syscall_ex(Event e, Context* c);
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1:printf("yield here!\n");break;
+    case 1:printf("yield here!\n");schedule(c);break;
     case 2:do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
