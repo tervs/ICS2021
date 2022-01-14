76e5ea33bb6df72b5576e27d180bddc1c6bdef3d
tracer-ics2021 <tracer@njuics.org>
2022-01-15 00:59:09 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  00:59:09 up 2 days,  1:02,  1 user,  load average: 2.38, 1.13, 0.66

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,14 +7,14 @@ int syscall_ex(Event e, Context* c);
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1:printf("yield here!\n");return schedule(c);break;
+    case 1:return schedule(c);break;
     case 2:do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
   return c;
 }
-
+//printf("yield here!\n");
 void init_irq(void) {
   Log("Initializing interrupt/exception handler...");
   cte_init(do_event);
