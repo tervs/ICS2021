1c9719a890396b628aa5ac420837357a6957c87b
tracer-ics2021 <tracer@njuics.org>
2022-01-14 19:30:41 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:30:41 up 1 day, 19:34,  1 user,  load average: 1.54, 1.19, 0.90

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -52,7 +52,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, 0xb; ecall");
+  asm volatile("li a7, 0x4; ecall");
   //printf("0x%08x",gpr(17));
 }
 
