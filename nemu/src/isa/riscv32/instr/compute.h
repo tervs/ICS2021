a3bdd5dbd7b2bef7633c31664e527406e5b8ad59
tracer-ics2021 <tracer@njuics.org>
2022-01-14 19:32:11 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:32:11 up 1 day, 19:35,  1 user,  load average: 0.99, 1.14, 0.91

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -263,7 +263,7 @@ def_EHelper(csrrw) {
 def_EHelper(ecall) {
  
   //printf("0x%08x\n\n\n",s->pc);
-  //isa_reg_display();
+  isa_reg_display();
   //isa_raise_intr(gpr(17),&(s->pc),&(s->dnpc));
   isa_raise_intr(0x0000000b,&(s->pc),&(s->dnpc));//no more independent yield
   //printf("\n\n\n");
