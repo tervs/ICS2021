f3a15f46d8d896073a50322a01a17f9b2b008538
tracer-ics2021 <tracer@njuics.org>
2022-01-15 17:43:19 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: 896e8c15d2d34c54aec17998afd32831   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  17:43:19 up 2 days, 17:46,  1 user,  load average: 0.68, 0.66, 0.49

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -70,11 +70,11 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
-   //Context *c = kstack.end - sizeof(Context);
-  //*c = (Context) { 0 };
-  //c->mepc=(uintptr_t)entry;
+  Context *c = kstack.end - sizeof(Context);
+  *c = (Context) { 0 };
+  c->mepc=(uintptr_t)entry;
   //c->gpr[10]=(intptr_t)arg;
   //printf("Ucontext pc %x\n",c->mepc);
-  //return c;
-  return NULL;
+  return c;
+  //return NULL;
 }
