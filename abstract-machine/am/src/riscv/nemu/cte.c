3fe64c2cbe6444061554146a5f3eaf5fb6a01199
tracer-ics2021 <tracer@njuics.org>
2022-01-16 19:45:41 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d7f5569ca2b449b7a334c8dee898b123   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:45:41 up  7:39,  1 user,  load average: 1.33, 0.88, 0.71

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -76,11 +76,12 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   //assert(0);
-  assert(0);
+  //assert(0);
     Context *c = kstack.end - sizeof(Context);
   *c = (Context) { 0 };
   c->mepc=(uintptr_t)entry;
   c->gpr[10]=(intptr_t)arg;
+  assert(0);
   //printf("mepc  %x\n",c->mepc);
   return c;
   //return NULL;
