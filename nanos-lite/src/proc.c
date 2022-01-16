7ff0c93e76a199d3a2026ce1943d6d5c74f320b1
tracer-ics2021 <tracer@njuics.org>
2022-01-16 19:45:24 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d7f5569ca2b449b7a334c8dee898b123   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  19:45:24 up  7:39,  1 user,  load average: 0.99, 0.81, 0.68

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -11,7 +11,7 @@ void context_kload(PCB* pcb,void(*entry)(void*),void *arg){
   //assert(0);
   //printf("entry %x\n",entry);
   Area stack = {pcb->stack,pcb->stack + STACK_SIZE};
-  assert(0);
+  //assert(0);
   //printf("stack:%p->%p\n",stack.start,stack.end);
   pcb->cp=kcontext(stack,entry,arg);
   //printf("pcb->cp:%p\n",pcb->cp);
