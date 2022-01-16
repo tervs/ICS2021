52859f0159d0b197e219635219eb2e88a8c18e27
tracer-ics2021 <tracer@njuics.org>
2022-01-16 21:56:31 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d7f5569ca2b449b7a334c8dee898b123   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  21:56:31 up  9:50,  1 user,  load average: 0.51, 0.62, 0.60

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -76,10 +76,10 @@ Context* schedule(Context *prev) {
 current->cp = prev;
 //printf("prev %x\n",prev);
 // always select pcb[0] as the new process
-current = &pcb[0];
+//current = &pcb[0];
 Log("process time interrupt");
 //assert(0);
-//current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 //printf("pcb0 %x\n",&pcb[0]);
 //printf("return from sch  %x\n",current->cp);
 // then return the new context
