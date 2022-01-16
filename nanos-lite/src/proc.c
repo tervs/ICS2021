e433026fa08347cc6f4202b56ffd1293ea9cc8d2
tracer-ics2021 <tracer@njuics.org>
2022-01-16 22:29:41 +0800

    >  compile 201240077    Static hostname: yu-Lenovo-Legion-R7000P2020H          Icon name: computer-laptop            Chassis: laptop         Machine ID: 8c8d3e6adfc14f3cbd68e7bd7c2276c9            Boot ID: d7f5569ca2b449b7a334c8dee898b123   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-46-generic       Architecture: x86-64  22:29:41 up 10:23,  1 user,  load average: 1.21, 0.89, 0.68

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -56,7 +56,7 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)0xffffffff);
   //assert(0);
   //while(1);
-  context_uload(&pcb[1], "/bin/pal",NULL,NULL);
+  //context_uload(&pcb[1], "/bin/pal",NULL,NULL);
 
   //assert(0);
   switch_boot_pcb();
